a9d20c9df0fde55635f57da15a8874f649e8eb46
developerkrishnasoftweb <developer.krishnasoftweb@gmail.com>
2020-12-16 15:34:25 +0530

    made changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:pal/Pages/Others/change_address.dart';
-import 'package:pal/Pages/Others/complain.dart';
 import 'package:pal/Pages/Others/earned_points.dart';
-import 'package:pal/Pages/Others/kyc_details.dart';
-import 'package:pal/Pages/Others/product_description.dart';
-import 'package:pal/Pages/Others/redeem_gift.dart';
 
 void main() {
   runApp(MyApp());
