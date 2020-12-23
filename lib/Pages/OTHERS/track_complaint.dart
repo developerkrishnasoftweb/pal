d3f3d5c00275f42735b582f96f3bb47b0f0bbe57
developerkrishnasoftweb <developer.krishnasoftweb@gmail.com>
2020-12-23 10:04:20 +0530

    completed track complaint screen

--- lib/Pages/OTHERS/track_complaint.dart
+++ lib/Pages/OTHERS/track_complaint.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:pal/Constant/color.dart';
 import 'package:pal/SERVICES/services.dart';
 import 'package:pal/SERVICES/urls.dart';
 import '../../Common/appbar.dart';
@@ -17,17 +18,30 @@ class TrackComplaint extends StatefulWidget {
 class _TrackComplaintState extends State<TrackComplaint> {
   String complainNo = "";
   ComplainDetails details;
+  bool isLoading = false;
   _getComplainData() async {
+    details = null;
     if(complainNo.isNotEmpty){
+      setState(() {
+        isLoading = true;
+      });
       Services.trackComplaint(FormData.fromMap({"api_key" : Urls.apiKey, "ticket_no" : complainNo})).then((value) {
         if(value.response == "y"){
-          print(value.data[0]["progress"]);
-          details = ComplainDetails(complainNo: value.data[0]["detail"]["ticket_no"], status: value.data[0]["detail"]["status"], desc: value.data[0]["detail"]["description"], progress: value.data[0]["progress"]);
+          setState(() {
+            details = ComplainDetails(complainNo: value.data[0]["detail"]["ticket_no"], status: value.data[0]["detail"]["status"], desc: value.data[0]["detail"]["description"], progress: value.data[0]["progress"]);
+            isLoading = false;
+          });
         } else {
+          setState(() {
+            isLoading = false;
+          });
           Fluttertoast.showToast(msg: value.message);
         }
       });
     } else {
+      setState(() {
+        isLoading = false;
+      });
       Fluttertoast.showToast(msg: "Please enter complain no.");
     }
   }
@@ -36,24 +50,48 @@ class _TrackComplaintState extends State<TrackComplaint> {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: appBar(context: context, title: "Track Complaint"),
-      body: Column(
-        children: [
-          input(
-            autoFocus: true,
-            context: context,
-            text: "Enter Complaint No.",
-            padding: EdgeInsets.all(20),
-            decoration: InputDecoration(
-              border: border(),
+      body: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        padding: EdgeInsets.only(bottom: 100),
+        child: Column(
+          children: [
+            input(
+              autoFocus: true,
+              context: context,
+              text: "Enter Complaint No.",
+              padding: EdgeInsets.all(20),
+              decoration: InputDecoration(
+                border: border(),
+              ),
+              onEditingComplete: _getComplainData,
+              onChanged: (value){
+                setState(() {
+                  complainNo = value;
+                });
+              }
             ),
-            onEditingComplete: _getComplainData,
-            onChanged: (value){
-              setState(() {
-                complainNo = value;
-              });
-            }
-          ),
-        ],
+            SizedBox(height: 10,),
+            details != null ? ExpansionTile(
+              title: Text("Complain No.:" + details.complainNo),
+              initiallyExpanded: true,
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Issue : " + details.desc),
+                  Text("Status : " + details.status),
+                ],
+              ),
+              childrenPadding: EdgeInsets.only(left: 20),
+              children: [
+                Divider(endIndent: 20,),
+                Align(child: Text("Progress", style: Theme.of(context).textTheme.bodyText1.copyWith(fontWeight: FontWeight.bold, fontSize: 17)), alignment: Alignment.centerLeft,),
+                for(int i = 0; i < details.progress.length; i++)...[
+                  Container(child: Text(details.progress[i]["comment"]), padding: EdgeInsets.symmetric(vertical: 10), alignment: Alignment.centerLeft,),
+                ]
+              ],
+            ) : isLoading ? SizedBox(height: 30, width: 30, child: CircularProgressIndicator(valueColor: AlwaysStoppedAnimation(AppColors.primaryColor),),) : SizedBox()
+          ],
+        ),
       ),
       floatingActionButton: complainNo.isNotEmpty ? customButton(context: context, onPressed: _getComplainData, height: 60, width: size.width, text: "SEARCH") : null,
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
@@ -62,10 +100,6 @@ class _TrackComplaintState extends State<TrackComplaint> {
 }
 class ComplainDetails{
   final String complainNo, desc, status;
-  final List<Progress> progress;
+  final List<dynamic> progress;
   ComplainDetails({this.status, this.desc, this.complainNo, this.progress});
-}
-class Progress{
-  final String id, comment, dateTime, lastModifyDate;
-  Progress({this.dateTime, this.id, this.comment, this.lastModifyDate});
 }
