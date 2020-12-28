43e8c5a71992507dd653c4fccb5918c4739ee3ba
developerkrishnasoftweb <developer.krishnasoftweb@gmail.com>
2020-12-28 12:50:15 +0530

    completed video player

--- lib/UI/PRODUCTS/product_preview.dart
+++ lib/UI/PRODUCTS/product_preview.dart
@@ -13,51 +13,78 @@ class ProductPreview extends StatefulWidget {
 
 class _ProductPreviewState extends State<ProductPreview> {
   VideoPlayerController _controller;
-  bool isPlaying = true;
+  bool isPlaying = false;
   String baseUrl = "https://generalstore.krishnasoftweb.com/";
-  Duration  position = Duration();
+  Duration position = Duration();
   @override
   void initState() {
     super.initState();
-    _controller = VideoPlayerController.network(baseUrl + widget.path)..initialize().then((_) {setState(() {
-      _controller.play();
-      _controller.setVolume(1.0);
-      _controller.setLooping(true);
-    });});
-    _controller.addListener(() {setState(() {
-
-    });});
+    _controller = VideoPlayerController.network(baseUrl + widget.path)
+      ..initialize().then((_) {
+        setState(() {
+          _controller.play();
+          _controller.setVolume(1.0);
+        });
+      });
+    _controller.addListener(() {
+      setState(() {});
+    });
   }
+
   @override
   void dispose() {
     super.dispose();
     _controller.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: appBar(context: context, title: "Product Preview"),
-      body: Center(
-        child: _controller.value.initialized ? Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        appBar: appBar(context: context, title: "Product Preview"),
+        body: Center(
+            child: Stack(
+              alignment: Alignment.center,
           children: [
-            AspectRatio(
-              aspectRatio: 16 / 9,
-              child: VideoPlayer(_controller),
-            ),
-            VideoProgressIndicator(_controller, allowScrubbing: true),
+            _controller.value.initialized
+                ? GestureDetector(
+                  onTap: (){
+                    if (_controller.value.isPlaying) {
+                      _controller.pause();
+                      setState(() {
+                        isPlaying = true;
+                      });
+                    } else {
+                      _controller.initialize();
+                      _controller.play();
+                      setState(() {
+                        isPlaying = false;
+                      });
+                    }
+                  },
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        AspectRatio(
+                          aspectRatio: 16 / 9,
+                          child: VideoPlayer(_controller),
+                        ),
+                        VideoProgressIndicator(_controller, allowScrubbing: true),
+                      ],
+                    ),
+                )
+                : SizedBox(
+                    height: 30,
+                    width: 30,
+                    child: CircularProgressIndicator(
+                      valueColor:
+                          AlwaysStoppedAnimation(AppColors.primaryColor),
+                    ),
+                  ),
+            isPlaying ? Align(
+              alignment: Alignment.center,
+              child: Icon(Icons.play_arrow, size: 50, color: Colors.white,),
+            ) : SizedBox()
           ],
-        ) : SizedBox(height: 30, width: 30, child: CircularProgressIndicator(valueColor: AlwaysStoppedAnimation(AppColors.primaryColor),),),
-      ),
-      floatingActionButton: _controller.value.initialized ? FloatingActionButton(backgroundColor: AppColors.primaryColor, elevation: 0, onPressed: _controller.value.isBuffering ? null : () {
-        if(_controller.value.isPlaying) {
-          _controller.pause();
-        } else {
-          _controller.initialize();
-          _controller.play();
-        }
-        print(_controller.value.duration.inSeconds);
-      }, child: _controller.value.isBuffering ? SizedBox(height: 25, width: 25, child: CircularProgressIndicator(valueColor: AlwaysStoppedAnimation(Colors.white),),) : Icon(_controller.value.isPlaying ? Icons.pause : Icons.play_arrow),) : null
-    );
+        )),);
   }
 }
