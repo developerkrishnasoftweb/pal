a90a6da064b8b36bdc20247d36f69973a8dee2c1
developerkrishnasoftweb <developer.krishnasoftweb@gmail.com>
2020-12-28 10:23:49 +0530

    made changes to product_review.dart

--- lib/UI/OTHERS/product_review.dart
+++ lib/UI/OTHERS/product_review.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:pal/Common/custom_button.dart';
 import 'package:pal/Common/input_decoration.dart';
 import 'package:pal/Common/textinput.dart';
 import '../../Common/appbar.dart';
@@ -12,6 +13,7 @@ class ProductReview extends StatefulWidget {
 
 class _ProductReviewState extends State<ProductReview> {
   int feedback = 0;
+  String review = "";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,10 +33,11 @@ class _ProductReviewState extends State<ProductReview> {
               },
             ),
           ),
-          input(context: context, decoration: InputDecoration(border: border()), maxLines: 5, text: "Review"),
+          input(context: context, decoration: InputDecoration(border: border()), maxLines: 5, text: "Review", onChanged: (value) => setState(() => review = value)),
         ],
       ),
-
+      floatingActionButton: feedback > 0 && review.isNotEmpty ? customButton(context: context, onPressed: (){}, text: "SUBMIT") : null,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
