2401ae21e4d59e0d41c3955bcd8e1403cfd5c860
developerkrishnasoftweb <developer.krishnasoftweb@gmail.com>
2020-12-30 11:28:39 +0530

    design completed of notification.dart

--- lib/UI/OTHERS/notification.dart
+++ lib/UI/OTHERS/notification.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:pal/Common/appbar.dart';
+import '../../Common/appbar.dart';
 
 class Notifications extends StatefulWidget {
   @override
@@ -10,8 +10,26 @@ class Notifications extends StatefulWidget {
 class _NotificationState extends State<Notifications> {
   List<NotificationData> notifications = [
     NotificationData(
-        name: "Notification 1",
-        image: "https://i.pinimg.com/originals/51/f6/fb/51f6fb256629fc755b8870c801092942.png",
+        title: "Notification 1",
+        image: "assets/icons/store-icon.jpg",
+        id: "1",
+        time: "2 days ago",
+        message: "Hi!, How are you! \nThe quick brown fox jumps over the lazy dog"),
+    NotificationData(
+        title: "Notification 1",
+        image: "assets/icons/store-icon.jpg",
+        id: "1",
+        time: "2 days ago",
+        message: "Hi!, How are you! \nThe quick brown fox jumps over the lazy dog"),
+    NotificationData(
+        title: "Notification 1",
+        image: "assets/icons/store-icon.jpg",
+        id: "1",
+        time: "2 days ago",
+        message: "Hi!, How are you! \nThe quick brown fox jumps over the lazy dog"),
+    NotificationData(
+        title: "Notification 1",
+        image: "assets/icons/store-icon.jpg",
         id: "1",
         time: "2 days ago",
         message: "Hi!, How are you! \nThe quick brown fox jumps over the lazy dog"),
@@ -24,43 +42,49 @@ class _NotificationState extends State<Notifications> {
         physics: BouncingScrollPhysics(),
         child: Column(
           children: [
-            ExpansionTile(
-              leading: Image.network(notifications[0].image),
-              title: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(notifications[0].name, style: Theme.of(context).textTheme.bodyText1.copyWith(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.grey),),
-                  Text(notifications[0].time, style: Theme.of(context).textTheme.bodyText1.copyWith(fontSize: 12, fontWeight: FontWeight.bold, color: Colors.grey),)
-                ],
-              ),
-              tilePadding: notifications[0].expanded ? EdgeInsets.symmetric(horizontal: 16, vertical: 8) : null,
-              subtitle: notifications[0].expanded ? null : Text(notifications[0].message, overflow: TextOverflow.ellipsis, maxLines: 1,),
-              onExpansionChanged: (value) {
-                setState(() {
-                  notifications[0].expanded = value;
-                });
-              },
-              initiallyExpanded: false,
-              expandedCrossAxisAlignment: CrossAxisAlignment.start,
-              childrenPadding: EdgeInsets.symmetric(vertical: 5, horizontal: 16),
-              children: notifications[0].expanded ? [
-                Divider(),
-                Text(notifications[0].message)
-              ] : [],
-            )
+            for(int i = 0; i < notifications.length; i++)
+              notificationBuilder(notifications[i])
           ],
         ),
       ),
     );
   }
 
-  Widget notificationBuilder() {
-    return null;
+  Widget notificationBuilder(NotificationData notification) {
+    return Card(
+      elevation: 1,
+      margin: EdgeInsets.all(10),
+      shadowColor: Colors.grey[100],
+      child: ExpansionTile(
+        leading: ClipRRect(borderRadius: BorderRadius.circular(5), child: Image.asset(notification.image, fit: BoxFit.fill, height: 50, width: 50,)),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(notification.title, style: Theme.of(context).textTheme.bodyText1.copyWith(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.grey),),
+            Text(notification.time, style: Theme.of(context).textTheme.bodyText1.copyWith(fontSize: 12, fontWeight: FontWeight.bold, color: Colors.grey),)
+          ],
+        ),
+        tilePadding: notification.expanded ? EdgeInsets.symmetric(horizontal: 16, vertical: 8) : null,
+        subtitle: notification.expanded ? null : Text(notification.message, overflow: TextOverflow.ellipsis, maxLines: 1,),
+        onExpansionChanged: (value) {
+          setState(() {
+            notification.expanded = value;
+          });
+        },
+        initiallyExpanded: false,
+        expandedCrossAxisAlignment: CrossAxisAlignment.start,
+        childrenPadding: EdgeInsets.symmetric(vertical: 5, horizontal: 16),
+        children: notification.expanded ? [
+          Divider(),
+          Text(notification.message)
+        ] : [],
+      ),
+    );
   }
 }
 
 class NotificationData {
-  final String image, name, message, id, time;
+  final String image, title, message, id, time;
   bool expanded;
-  NotificationData({this.message, this.name, this.image, this.id, this.expanded : false, this.time});
+  NotificationData({this.message, this.title, this.image, this.id, this.expanded : false, this.time});
 }
