2415d338c6a4c345d4712e3aa028bace230a7b2a
krishnasoftweb <developer.krishnasoftweb@gmail.com>
2021-01-09 15:14:52 +0530

    working on weekly updates

--- lib/UI/RETAILER_BONDING_PROGRAM/weekly_update.dart
+++ lib/UI/RETAILER_BONDING_PROGRAM/weekly_update.dart
@@ -15,21 +15,33 @@ class WeeklyUpdate extends StatefulWidget {
 class _WeeklyUpdateState extends State<WeeklyUpdate> {
   String cumulativePurchase = "0";
   int purchase = 0, earnedPoints = 0;
-  List<String> weekPoints = ["10"];
+  List<String> weekPoints = [];
   @override
   void initState() {
     getWeeklyReport();
     super.initState();
   }
+
   getWeeklyReport() async {
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     setState(() {
-      cumulativePurchase = jsonDecode(sharedPreferences.getString(UserParams.userData))[0][UserParams.totalOrder];
+      cumulativePurchase =
+          jsonDecode(sharedPreferences.getString(UserParams.userData))[0]
+              [UserParams.totalOrder];
     });
     Services.weeklyReport().then((value) {
-      if(value.response == "y") {
+      if (value.response == "y") {
         for (int i = 0; i < value.data[0]["earn"].length; i++) {
-          print(value.data[0]["earn"][(i + 1).toString()]);
+          for(int j = 0; j < value.data[0]["earn"]["${(i + 1)}"].length; j++) {
+            print(value.data[0]["earn"]["${(i + 1)}"][j]["point"]);
+            setState(() {
+              purchase += int.parse(value.data[0]["earn"][[(i + 1).toString()]][j]["purchase"]);
+              earnedPoints += int.parse(value.data[0]["earn"][[(i + 1).toString()]][j]["point"]);
+            });
+          }
+          setState(() {
+
+          });
           // for(int j = 0; j < value.data[0]["earn"][[(i + 1).toString()]].length; i++) {
           //   setState(() {
           //     purchase += int.parse(value.data[0]["earn"][[(i + 1).toString()]][j]["purchase"]);
@@ -47,6 +59,7 @@ class _WeeklyUpdateState extends State<WeeklyUpdate> {
       }
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -65,18 +78,27 @@ class _WeeklyUpdateState extends State<WeeklyUpdate> {
               child: Column(
                 children: [
                   buildRow(title: "Current Cycle :", value: "30 Nov - 27 Dec"),
-                  buildRow(title: "Purchase for this cycle :", value: purchase.toString()),
-                  buildRow(title: "Cumulative Purchase :", value: cumulativePurchase),
+                  buildRow(
+                      title: "Purchase for this cycle :",
+                      value: purchase.toString()),
+                  buildRow(
+                      title: "Cumulative Purchase :",
+                      value: cumulativePurchase),
                   buildRow(title: "Earned Points :", value: "19965"),
                 ],
               ),
             ),
-            SizedBox(height: 30,),
+            SizedBox(
+              height: 30,
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [for(int i = 0; i < weekPoints.length; i++)...[
-                weekBuilder(head: "WK ${(i + 1).toString()}", value: weekPoints[i]),
-              ]],
+              children: [
+                for (int i = 0; i < weekPoints.length; i++) ...[
+                  weekBuilder(
+                      head: "WK ${(i + 1).toString()}", value: weekPoints[i]),
+                ]
+              ],
             ),
             /* SizedBox(height: 30,),
             headerBuilder("Range Multiplier"),
@@ -98,7 +120,7 @@ class _WeeklyUpdateState extends State<WeeklyUpdate> {
     );
   }
 
-  Widget headerBuilder(String head){
+  Widget headerBuilder(String head) {
     Size size = MediaQuery.of(context).size;
     return Container(
       height: 45,
@@ -108,40 +130,65 @@ class _WeeklyUpdateState extends State<WeeklyUpdate> {
         color: Color(0xff373737),
         borderRadius: BorderRadius.circular(10),
       ),
-      child: Text(head, style: Theme.of(context).textTheme.bodyText1.copyWith(fontWeight: FontWeight.bold, color: Colors.white), overflow: TextOverflow.ellipsis,),
+      child: Text(
+        head,
+        style: Theme.of(context)
+            .textTheme
+            .bodyText1
+            .copyWith(fontWeight: FontWeight.bold, color: Colors.white),
+        overflow: TextOverflow.ellipsis,
+      ),
     );
   }
 
-  Widget dataTable(List<Widget> row){
+  Widget dataTable(List<Widget> row) {
     Size size = MediaQuery.of(context).size;
     return Container(
-      width: size.width,
-      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-      alignment: Alignment.center,
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          border: Border.all(color: Color(0xff373737).withOpacity(0.5), width: 0)
-      ),
-      child: Column(
-        children: row,
-      )
-    );
+        width: size.width,
+        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(
+                color: Color(0xff373737).withOpacity(0.5), width: 0)),
+        child: Column(
+          children: row,
+        ));
   }
 
-  Widget dataTableRow({String title, String value}){
+  Widget dataTableRow({String title, String value}) {
     Size size = MediaQuery.of(context).size;
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 3.0),
       child: Row(
         children: [
-          Container(width: (size.width * 0.7) - 30, child: Text(title, style: Theme.of(context).textTheme.bodyText1.copyWith(fontSize: 12), softWrap: true,)),
-          Container(width: (size.width * 0.3) - 30, alignment: Alignment.centerRight, child: Text(value ?? "--/--", style: Theme.of(context).textTheme.bodyText1.copyWith(fontSize: 12), softWrap: true,)),
+          Container(
+              width: (size.width * 0.7) - 30,
+              child: Text(
+                title,
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyText1
+                    .copyWith(fontSize: 12),
+                softWrap: true,
+              )),
+          Container(
+              width: (size.width * 0.3) - 30,
+              alignment: Alignment.centerRight,
+              child: Text(
+                value ?? "--/--",
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyText1
+                    .copyWith(fontSize: 12),
+                softWrap: true,
+              )),
         ],
       ),
     );
   }
 
-  Widget weekBuilder({String head, String value}){
+  Widget weekBuilder({String head, String value}) {
     Size size = MediaQuery.of(context).size;
     return Column(
       children: [
@@ -153,30 +200,64 @@ class _WeeklyUpdateState extends State<WeeklyUpdate> {
             color: Color(0xff373737),
             borderRadius: BorderRadius.circular(10),
           ),
-          child: Text(head, style: Theme.of(context).textTheme.bodyText1.copyWith(fontWeight: FontWeight.bold, color: Colors.white), overflow: TextOverflow.ellipsis,),
+          child: Text(
+            head,
+            style: Theme.of(context)
+                .textTheme
+                .bodyText1
+                .copyWith(fontWeight: FontWeight.bold, color: Colors.white),
+            overflow: TextOverflow.ellipsis,
+          ),
         ),
-        SizedBox(height: 5,),
+        SizedBox(
+          height: 5,
+        ),
         Container(
           height: 45,
           width: (size.width - 50) / weekPoints.length,
           alignment: Alignment.center,
           decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            border: Border.all(color: Color(0xff373737).withOpacity(0.5))
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(color: Color(0xff373737).withOpacity(0.5))),
+          child: Text(
+            value ?? "-",
+            style: Theme.of(context)
+                .textTheme
+                .bodyText1
+                .copyWith(color: Colors.black),
+            overflow: TextOverflow.ellipsis,
           ),
-          child: Text(value ?? "-", style: Theme.of(context).textTheme.bodyText1.copyWith(color: Colors.black), overflow: TextOverflow.ellipsis,),
         )
       ],
     );
   }
 
-  Widget buildRow({String title, String value}){
+  Widget buildRow({String title, String value}) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 3.0),
       child: Row(
         children: [
-          Container(width: (MediaQuery.of(context).size.width * 0.6) - 30, child: Text(title, style: Theme.of(context).textTheme.bodyText1.copyWith(fontWeight: FontWeight.bold, fontSize: 15), softWrap: true,)),
-          Container(width: (MediaQuery.of(context).size.width * 0.4) - 30, alignment: Alignment.centerRight, child: Text(value, style: Theme.of(context).textTheme.bodyText1.copyWith(fontWeight: FontWeight.bold, fontSize: 15, color: Colors.grey), softWrap: true,)),
+          Container(
+              width: (MediaQuery.of(context).size.width * 0.6) - 30,
+              child: Text(
+                title,
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyText1
+                    .copyWith(fontWeight: FontWeight.bold, fontSize: 15),
+                softWrap: true,
+              )),
+          Container(
+              width: (MediaQuery.of(context).size.width * 0.4) - 30,
+              alignment: Alignment.centerRight,
+              child: Text(
+                value,
+                style: Theme.of(context).textTheme.bodyText1.copyWith(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15,
+                    color: Colors.grey),
+                softWrap: true,
+              )),
         ],
       ),
     );
