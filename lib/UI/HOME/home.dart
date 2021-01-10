d5e36a9a90780e690cb68532bcdf30604d522670
krishnasoftweb <developer.krishnasoftweb@gmail.com>
2021-01-10 10:34:00 +0530

    removed unused codes

--- lib/UI/HOME/home.dart
+++ lib/UI/HOME/home.dart
@@ -105,7 +105,7 @@ class _HomeState extends State<Home> {
                   if(this.rate < 3) {
                     rateMessage = "Good";
                   } else {
-                    rateMessage = "Good";
+                    rateMessage = "Excellent";
                   }
                 });
               },),
