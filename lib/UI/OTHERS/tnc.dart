9050522fdc644de6f386f7c819b5bcfce57b08df
developerkrishnasoftweb <developer.krishnasoftweb@gmail.com>
2021-02-16 11:46:45 +0530

    made changes

--- lib/UI/OTHERS/tnc.dart
+++ lib/UI/OTHERS/tnc.dart
@@ -1,12 +1,9 @@
-import 'dart:convert';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:pal/Common/appbar.dart';
 import 'package:pal/Constant/global.dart';
 import 'package:pal/Constant/strings.dart';
 import 'package:pal/LOCALIZATION/localizations_constraints.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 
 class TermsNCondition extends StatefulWidget {
   @override
@@ -14,27 +11,14 @@ class TermsNCondition extends StatefulWidget {
 }
 
 class _TermsNConditionState extends State<TermsNCondition> {
-  String title = "";
-  WebViewController _controller;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: appBar(
-          context: context,
-          title: translate(context, LocaleStrings.termsAndConditions)),
-      body: WebView(
-        initialUrl: 'about:blank',
-        onWebViewCreated: (WebViewController webViewController) {
-          _controller = webViewController;
-          _loadHtmlFromAssets();
-        },
+      appBar: appBar(context: context, title: translate(context, LocaleStrings.termsAndConditions)),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+        child: Text(removeHtmlTags(data: config.terms)),
       ),
     );
-  }
-
-  _loadHtmlFromAssets() async {
-    _controller.loadUrl(Uri.dataFromString(config.terms,
-            mimeType: 'text/html', encoding: Encoding.getByName('utf-8'))
-        .toString());
   }
 }
